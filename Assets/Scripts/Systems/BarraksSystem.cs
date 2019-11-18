4ab0837ad6185dccc518e74898df995a9ca837d6
max31000 <49785937+max31000@users.noreply.github.com>
2019-11-18 22:03:46 +0500

    [del] удалил все мета файлы, из за них все летело в пизду

---
+++ Assets/Scripts/Systems/BarraksSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Leopotam.Ecs;
+using Components;
+
+namespace Systems
+{
+    public class BarraksSystem : IEcsRunSystem
+    {
+        private readonly GameDefinitions gameDefinitions = null;
+        private readonly EcsWorld world = null;
+        private readonly EcsFilter<UnitCreateEvent> unitCreateEvents = null;
+
+        public void Run()
+        {
+            for (var i = 0; i < unitCreateEvents.GetEntitiesCount(); i++)
+            {
+                
+            }
+        }
+    }
+}
