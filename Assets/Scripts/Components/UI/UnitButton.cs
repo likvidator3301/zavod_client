7c762e906668748a173c59d56d57fa1a6d017043
Zergeker <zergec@yandex.ru>
2019-12-16 13:01:19 +0500

    Added nullchecking for unitbutton attributes

--- Assets/Scripts/Components/UI/UnitButton.cs
+++ Assets/Scripts/Components/UI/UnitButton.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Leopotam.Ecs;
 
 namespace Components
 {
     public class UnitButton
     {
+        [EcsIgnoreNullCheck]
         public ButtonComponent Button;
+        [EcsIgnoreNullCheck]
         public UnitComponent Unit;
     }
 }
